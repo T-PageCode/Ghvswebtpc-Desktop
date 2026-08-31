9cdecd2490e4c84ab65cf660aaeb08340164e86a
T-PageCode Developer <cdt20260@outlook.com>
2026-08-31 19:36:12 +0800

    1.0.8:桌面综合更新

--- WebSystem/Script/Desktop.js
+++ WebSystem/Script/Desktop.js
@@ -28,7 +28,7 @@ function IfWindowBorderRadius() {
     let Window = document.querySelectorAll(".window");
     if (BorderRadius === "True") {
         Window.forEach(Win => {
-            Win.style.borderRadius = "10px";
+            Win.style.borderRadius = "15px";
         })
     }
     else {
@@ -287,22 +287,22 @@ function QHImage() {
     }
 }
 QHImage();
-function IfDBRWLWhiteDivShow() {
-    let DBLWhiteDivLocalStorage = localStorage.getItem("ShowDBLWhiteDiv") || "True";
-    let DBL = document.getElementById("div_id1");
+function IftaskbarWLWhiteDivShow() {
+    let taskbarWhiteDivLocalStorage = localStorage.getItem("ShowDBLWhiteDiv") || "True";
+    let taskbar = document.getElementById("taskbar");
     let DivClass1 = document.querySelectorAll(".div_class1")
-    if (DBLWhiteDivLocalStorage === "True") {
-        DBL.style.backgroundColor = "white";
+    if (taskbarWhiteDivLocalStorage === "True") {
+        taskbar.style.backgroundColor = "white";
         DivClass1.backgroundColor = "white";
     }
-    else if (DBLWhiteDivLocalStorage === "False") {
-        DBL.style.backgroundColor = "rgba(255,255,255,0.0)";
+    else if (taskbarWhiteDivLocalStorage === "False") {
+        taskbar.style.backgroundColor = "rgba(255,255,255,0.0)";
         DivClass1.forEach(DivClass1Item => {
             DivClass1Item.style.backgroundColor = "rgba(255,255,255,0.0)";
         })
     }
 }
-IfDBRWLWhiteDivShow();
+IftaskbarWLWhiteDivShow();
 function ErrorCode(Code) {
     localStorage.setItem("ErrorCode", Code);
 }
